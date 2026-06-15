040773e11a50d5526daaa191d093792ccfeccb61
Your Name <sarahsaeed0990@gmail.com>
2026-06-15 23:59:36 +0530

    new

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  res.json({
-    message: "Register route working 🚀",
-  });
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully 🚀",
+      user,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
 });
 
 module.exports = router;
