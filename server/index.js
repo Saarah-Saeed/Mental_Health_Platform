318594c883e5fc084799a1b9adf14b5edbd71395
Your Name <sarahsaeed0990@gmail.com>
2026-06-15 23:30:44 +0530

    index.js change

--- server/index.js
+++ server/index.js
@@ -3,24 +3,32 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Connect Database
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Test Routes
 app.get("/", (req, res) => {
   res.send("Mental Health Backend Running 🚀");
 });
 
 app.get("/api/test", (req, res) => {
   res.json({
-    message: "Backend connected successfully 🚀"
+    message: "Backend connected successfully 🚀",
   });
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
