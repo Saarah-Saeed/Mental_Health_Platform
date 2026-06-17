5bb43a5b9ece9b5cbbe3d53f831357044b77b750
Your Name <sarahsaeed0990@gmail.com>
2026-06-17 11:59:46 +0530

    Navbar.jsx

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -5,12 +5,15 @@ function Navbar() {
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-bold text-blue-600">MindSpace 🌿</h1>
 
-      <div className="space-x-6">
-        <Link className="text-gray-700 hover:text-blue-500" to="/">Home</Link>
-        <Link className="text-gray-700 hover:text-blue-500" to="/community">Community</Link>
-        <Link className="text-gray-700 hover:text-blue-500" to="/book">Book</Link>
-        <Link className="text-gray-700 hover:text-blue-500" to="/stories">Stories</Link>
-      </div>
+     <div className="space-x-6">
+      <Link className="text-gray-700 hover:text-blue-500" to="/">Home</Link>
+      <Link className="text-gray-700 hover:text-blue-500" to="/community">Community</Link>
+      <Link className="text-gray-700 hover:text-blue-500" to="/book">Book</Link>
+      <Link className="text-gray-700 hover:text-blue-500" to="/stories">Stories</Link>
+      <Link className="text-gray-700 hover:text-blue-500" to="/create-story">
+       Create Story
+       </Link>
+     </div>
     </nav>
   );
 }
