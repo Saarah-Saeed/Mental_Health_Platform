ea6eb23f9d5f5ab603a8e205cf602a134b898f8f
Your Name <sarahsaeed0990@gmail.com>
2026-06-18 01:53:22 +0530

    Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,13 +4,44 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await fetch(
+      "http://localhost:5000/api/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    alert(data.message);
+
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-xl">
       <h1 className="text-3xl font-bold mb-6 text-center">
         Login 🔐
       </h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           className="w-full border p-3 rounded mb-4"
           type="email"
