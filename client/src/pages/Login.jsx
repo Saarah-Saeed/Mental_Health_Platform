3ba75c1c3a58d23209fc83b0d0971ae8703a3c89
Your Name <sarahsaeed0990@gmail.com>
2026-06-19 00:59:30 +0530

    update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -45,7 +45,7 @@ function Login() {
         <input
           className="w-full border p-3 rounded mb-4"
           type="email"
-         
+          placeholder="Enter email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
